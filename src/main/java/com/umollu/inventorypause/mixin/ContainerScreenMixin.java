ed08ca05f0e92b34d5023c771b9f0cee6c676668
umollu <umollu@gmail.com>
2020-05-10 14:54:56 +0100

    Updated mod to 1.15.2;

--- src/main/java/com/umollu/inventorypause/mixin/ContainerScreenMixin.java
+++ src/main/java/com/umollu/inventorypause/mixin/ContainerScreenMixin.java
@@ -1,11 +1,11 @@
 package com.umollu.inventorypause.mixin;
 
-import net.minecraft.client.gui.ContainerProvider;
-import net.minecraft.client.gui.ContainerScreen;
-import net.minecraft.client.gui.Screen;
-import net.minecraft.client.gui.ingame.AbstractPlayerInventoryScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
+import net.minecraft.client.gui.screen.ingame.ContainerProvider;
+import net.minecraft.client.gui.screen.ingame.ContainerScreen;
 import net.minecraft.container.Container;
-import net.minecraft.text.TextComponent;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,14 +14,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ContainerScreen.class)
 public abstract class ContainerScreenMixin<T extends Container> extends Screen implements ContainerProvider<T> {
 
-
-    protected ContainerScreenMixin(TextComponent textComponent_1) {
-        super(textComponent_1);
+    protected ContainerScreenMixin(Text title) {
+        super(title);
     }
+
     @Inject(method = "isPauseScreen", at = @At("HEAD"), cancellable = true)
     public void isPauseScreen(CallbackInfoReturnable cir) {
 
-        if((Object)this instanceof AbstractPlayerInventoryScreen)
+        if((Object)this instanceof AbstractInventoryScreen)
         {
             cir.setReturnValue(true);
             cir.cancel();
