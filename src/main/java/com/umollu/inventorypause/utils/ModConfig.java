4f5efb8997007cbde278dcf649660a782979d188
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2021-02-05 17:08:46 +0100

    Rename language namespace from inventory-pause to inventorypause

--- src/main/java/com/umollu/inventorypause/utils/ModConfig.java
+++ src/main/java/com/umollu/inventorypause/utils/ModConfig.java
@@ -3,7 +3,7 @@ package com.umollu.inventorypause.utils;
 import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
 
-@Config(name = "inventory-pause")
+@Config(name = "inventorypause")
 public class ModConfig implements ConfigData {
     public boolean pauseInventory = true;
     public boolean pauseFurnace = false;
